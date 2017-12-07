a90cc7021d76ab4302127d3ac29a4fb0f8cb8a0d
ejaguete <erica.aguete@gmail.com>
2017-12-06 22:01:15 -0800

    did stuff i forgot what i did

--- convolve.cpp
+++ convolve.cpp
@@ -2,35 +2,48 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <cstdint>
+#include <vector>
 using std::fstream;
-using std::string;
 using std::cout;
 using std::endl;
+using std::vector;
 
-typedef struct WAV_HEADER
-{
+typedef struct RIFF_CHUNK {
 	// RIFF chunk descriptor
-	uint8_t 		RIFF[4];					// RIFF header
-	uint32_t	ChunkSize;			// RIFF chunk size
-	uint8_t		WAVE[4];				// WAVE header
+	uint8_t 	RIFF[4];						// RIFF header
+	uint32_t	ChunkSize;						// full chunk size
+	uint8_t		WAVE[4];						// WAVE header
+} riff_chk;
+
+typedef struct FMT_CHUNK {
 	// fmt subchunk
-	uint8_t		fmt[2];					// FMT header
-	uint32_t	Subchunk1Size;	// size of fmt chunk
-	uint16_t	AudioFormat;		// audio format
-	uint16_t	NumChannels;		// number of channels
-	uint32_t	SamplesPerSec;	// sampling frequency (Hz)
-	uint32_t	BytesPerSec;		// bytes per second
-	uint16_t	blockAlign;				// 2=16bit mono, 4 = 16bit stereo
-	uint16_t	bitsPerSample;		// number of bits per sample
-	uint16_t 	extra;						// in case chunk size is more than 16
-	// data subchunk
-	uint8_t		Subchunk2ID[4];	// "data" string
-	uint32_t	Subchunk2Size;	// sampled data length
+	uint8_t		fmt[4];							// FMT header
+	uint32_t	Subchunk1Size;					// size of fmt chunk
+	uint16_t	AudioFormat;					// audio format
+	uint16_t	NumChannels;					// number of channels
+	uint32_t	SamplesPerSec;					// sampling frequency (Hz)
+	uint32_t	BytesPerSec;					// bytes per second
+	uint16_t	blockAlign;						// 2=16bit mono, 4 = 16bit stereo
+	uint16_t	bitsPerSample;					// number of bits per sample
+} fmt_chk;
+
+typedef struct DATA_CHUNK {
+	uint8_t		Subchunk2ID[4];					// "data" string
+	uint32_t	Subchunk2Size;					// sampled data length
+} data_chk;
+
+typedef struct WAV_HEADER {
+	riff_chk riff;
+	fmt_chk fmt;
+	data_chk data;
 } wav_hdr;
 
+
 int getFileSize(FILE *input);
+void print_meta(int filelength, wav_hdr header);
+void parseWavFile(const char wavFile[], vector<float> signal, bool doScaling);
+void convolve(float x[], int N, float h[], int M, float y[], int P);
 
 int main(int argc , char *argv[]) {
 
@@ -42,64 +55,14 @@ int main(int argc , char *argv[]) {
 		char *irfile = argv[2];
 		char *outputfile = argv[3];
 		
-		wav_hdr wavHeader;
-		int headerSize = sizeof(wav_hdr), filelength = 0;
-		
 		printf("input file: %s\nIR file: %s\noutput file: %s\n",inputfile, irfile, outputfile);
 		
-		FILE * wavFile;
-		wavFile = fopen(inputfile, "r");
-		if(wavFile == NULL) {
-			fprintf(stderr, "Error opening file: %s\n", inputfile);
-			return 1;
-		} else {
-			// parse file
-			
-			// read header
-			size_t bytesRead = fread(&wavHeader, 1, headerSize, wavFile);
-			printf("header read %d bytes\n", bytesRead);
-			
-			if(bytesRead>0) {
-				
-				// read data
-				uint16_t bytespersample = wavHeader.bitsPerSample/8;
-				uint64_t numSamples = 
-				wavHeader.ChunkSize/bytespersample;		// how many samples in wav file
-				static const uint16_t BUFFER_SIZE = 4096;
-				int8_t *buffer = new int8_t[BUFFER_SIZE];
-				
-				// read data
-				while((bytesRead=fread(buffer, sizeof buffer[0], 
-				BUFFER_SIZE / (sizeof buffer[0]), wavFile)) > 0) {
-					// do smth with data
-					//printf("read %d bytes", bytesRead); 
-				}
-				
-				delete [] buffer;
-				buffer = nullptr;
-				filelength = getFileSize(wavFile);
-				
-				cout << "file is: 			" << filelength << " bytes" << endl;
-				cout << "RIFF header: 			\"" << wavHeader.RIFF[0] << wavHeader.RIFF[1] << wavHeader.RIFF[2] << wavHeader.RIFF[3] << "\"" << endl;
-				cout << "WAVE header: 			\"" << wavHeader.WAVE[0] << wavHeader.WAVE[1] << wavHeader.WAVE[2] << wavHeader.WAVE[3] << "\"" << endl;
-				cout << "fmt: 				\"" << wavHeader.fmt[0] << wavHeader.fmt[1] << wavHeader.fmt[2] << wavHeader.fmt[3] << "\"" << endl;
-				cout << "subchunk1 size: 		" << wavHeader.Subchunk1Size << endl;
-				cout << "data size: 			" << wavHeader.ChunkSize << endl;
-				
-				// sampling rate
-				cout << "sampling rate: 			" << wavHeader.SamplesPerSec << endl;
-				cout << "number of bits used: 		" << wavHeader.bitsPerSample << endl;
-				cout << "number of channels: 		" << wavHeader.NumChannels << endl;
-				cout << "number of bytes/second: 	" << wavHeader.BytesPerSec << endl;
-				cout << "data length: 			" << wavHeader.Subchunk2Size << endl;
-				cout << "audio format: 			" << wavHeader.AudioFormat << endl;
-				cout << "block align: 			" << wavHeader.blockAlign << endl;
-				cout << "data string: 			\"" << wavHeader.Subchunk2ID[0] << wavHeader.Subchunk2ID[1] << wavHeader.Subchunk2ID[2] << wavHeader.Subchunk2ID[3] << "\"" <<endl;
-			}
-			fclose(wavFile);
-			return 0;
-		}
+		float *input[], float *ir[];
+		input = parseWavFile(inputfile, input, true);
+		
+		return 0;
 	}
+	
 }
 
 int getFileSize(FILE *input) {
@@ -108,4 +71,88 @@ int getFileSize(FILE *input) {
 	fileSize = ftell(input);
 	fseek(input, 0, SEEK_SET);
 	return fileSize;
+}
+
+void print_meta(int filelength, wav_hdr header) {
+	// print wav file info
+	cout << "RIFF header: 			\"" << header.riff.RIFF[0] << header.riff.RIFF[1] << header.riff.RIFF[2] << header.riff.RIFF[3] << "\"" << endl;
+	cout << "chunk size: 			" << header.riff.ChunkSize << endl;
+	cout << "WAVE header: 			\"" << header.riff.WAVE[0] << header.riff.WAVE[1] << header.riff.WAVE[2] << header.riff.WAVE[3] << "\"" << endl;
+	
+	cout << "fmt: 				\"" << header.fmt.fmt[0] << header.fmt.fmt[1] << header.fmt.fmt[2] << header.fmt.fmt[3] << "\"" << endl;
+	cout << "subchunk1 size: 		" << header.fmt.Subchunk1Size << endl;
+	// sampling rate
+	cout << "sampling rate: 			" << header.fmt.SamplesPerSec << endl;
+	cout << "number of bits used: 		" << header.fmt.bitsPerSample << endl;
+	cout << "number of channels: 		" << header.fmt.NumChannels << endl;
+	cout << "number of bytes/second: 	" << header.fmt.BytesPerSec << endl;
+	cout << "audio format: 			" << header.fmt.AudioFormat << endl;
+	cout << "block align: 			" << header.fmt.blockAlign << endl;
+	
+	cout << "data string: 			\"" << header.data.Subchunk2ID[0] << header.data.Subchunk2ID[1] << header.data.Subchunk2ID[2] << header.data.Subchunk2ID[3] << "\"" <<endl;
+	cout << "data length: 			" << header.data.Subchunk2Size << endl;
+}
+
+void parseWavFile(const char wavFile[], vector<float> signal, bool doScaling) {
+	
+	FILE * wav;
+	wav = fopen(wavFile, "r");
+	if(wav == NULL) {
+		fprintf(stderr, "Error opening files: %s\n\n", wavFile);
+	} else {
+		int wavlength = 0;
+		wavlength = getFileSize(wav);
+		if(wavlength>0) {
+			wav_hdr header;
+			int riffSize = sizeof(riff_chk);
+			int fmtSize = sizeof(fmt_chk);
+			int dataSize = sizeof(data_chk);
+			
+			// parse input file
+			fread(&header.riff, 1, riffSize, wav);
+			fread(&header.fmt, 1, fmtSize, wav);
+			if(header.fmt.Subchunk1Size>16) {
+				fread(&header.fmt, 1, header.fmt.Subchunk1Size-16, wav);
+			}
+			fread(&header.data, 1, dataSize, wav);
+			
+			cout << "---------\nwav file\n---------" << endl;
+			print_meta(wavlength, header);
+			
+			uint16_t sampleSize = header.fmt.bitsPerSample;
+			uint64_t numSamples = header.data.Subchunk2Size/sampleSize/8;		// how many samples in wav file
+			int16_t BUFFER_LEN = numSamples;
+			
+			if(doScaling) {
+				for(int i=0; i<numSamples; i++) {
+					fread(&signal[i], 1, sampleSize, wav);
+					signal[i] /= (float) 32768.0;
+				}
+			} else {
+				for(int i=0; i<numSamples; i++) {
+					fread(&signal[i], 1, sampleSize, wav);
+				}
+			}
+		}
+	}
+}
+
+void convolve(float x[], int N, float h[], int M, float y[], int P) {
+	int n,m;
+	if(P!=(N+M-1)) {
+		cout << "output signal vector is wrong size\n" << endl;
+		cout << "is " << P << ", but should be " << N+M-1 << endl; 
+		cout << "aborting convolution" << endl;
+		return;
+	}
+	
+	for(n=0; n<P; n++) {
+		y[n] = 0.0;
+	}
+	
+	for(n=0; n<N; n++) {
+		for(m=0; n<M; m++) {
+			y[n+m] += x[n] * h[m];
+		}
+	}
 }
